d0cd05e700ce3e4501e36784fa79946236fa763f
MichaelOdons <michaelodonghanro287@gmail.com>
2024-11-26 11:11:11 +0100

    data added

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import sys
-import os
-
-# Add the 'src' folder to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-from src.config.configuration import main
+from src.mlProject.config.configuration import configuration
